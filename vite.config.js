a3132b2d0894ea3b0c48a62b5d46e45e422a771b
careackermane-prog <care_ackermane@unicah.edu>
2026-08-01 16:30:03 -0600

    Corregir base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_BASE_URL ?? '/' ,
+  base: '/pokedex-personalizado/',
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
 })
